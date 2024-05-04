73b910b2507253f6c81791cc5a3bcc7de40eebbc
NazarBoplan <baidyk63@gmail.com>
2024-05-04 12:44:54 +0200

    ))

--- Homework-4-JS/script.js
+++ Homework-4-JS/script.js
@@ -1,12 +1,28 @@
 const createNewUser = () => {
-    const newUser = {};
-    const firstName = prompt("Введіть ім'я:");
-    const lastName = prompt("Введіть прізвище:");
-    newUser.firstName = firstName;
-    newUser.lastName = lastName;
-     newUser.getLogin = () => (newUser.firstName.charAt(0) + newUser.lastName).toLowerCase();
-    return newUser; 
-};
-const user = createNewUser();
-console.log("Логін користувача: " + user.getLogin());
-console.log(newUser);
+    let firstName = prompt("Введіть ваше ім'я:");
+    let lastName = prompt("Введіть вашу фамілію:");
+    let newUser = {
+        firstName,
+        lastName,
+        getLogin() {
+            return (this.firstName.charAt(0) + this.lastName).toLowerCase();
+        },
+        setFirstName(newFirstName) {
+            this.firstName = newFirstName;
+        },
+        setLastName(newLastName) {
+            this.lastName = newLastName;
+        },
+    };
+    console.log(`Ім'я: ${newUser.firstName}`);
+    console.log(`Фамілія: ${newUser.lastName}`);
+    console.log(`Логін: ${ newUser.getLogin()}`);
+    return newUser;
+}
+
+let user = createNewUser();
+user.setFirstName("Тарас");
+user.setLastName("Шевченко");
+console.log(`Ім'я: ${user.firstName}`);
+console.log(`Фамілія: ${user.lastName}`);
+console.log(`Новий Логін: ${user.getLogin()}`);
