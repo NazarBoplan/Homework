5d32d8cbffe2faf9c85cc62448ecba08d58bd514
NazarBoplan <baidyk63@gmail.com>
2024-05-07 15:01:18 +0200

    ))

--- Homework-7-JS/script.js
+++ Homework-7-JS/script.js
@@ -1,34 +1,35 @@
-function displayList(array) {
-    const listContainer = document.getElementById('listContainer');
-    const listItems = array.map(item => {
+const list = (arr) => {
+    const listContainer = document.getElementById('listсontainer');
+
+    const renderlist = (item) => {
         if (Array.isArray(item)) {
-            // Если элемент массив, рекурсивно вызываем displayList для создания вложенного списка
-            return `<li><ul>${displayList(item)}</ul></li>`;
-        } else if (typeof item === 'object') {
-            // Если элемент объект, рекурсивно вызываем displayList для создания вложенного списка из его значений
-            return `<li><ul>${displayList(Object.values(item))}</ul></li>`;
+            const subitems = item.map(Item => renderlist(Item)).join('');
+            return `<ul>${subitems}</ul>`;
+        } else if (typeof item === 'object' && item !== null) {
+            const subitems = Object.entries(item).map(([key, value]) => `<li>${key}: ${value}</li>`).join('');
+            return `<ul>${subitems}</ul>`;
         } else {
-            // Иначе просто создаем элемент списка
             return `<li>${item}</li>`;
         }
-    }).join('');
+    };
+
+    const listItems = arr.map(item => renderlist(item)).join('');
     listContainer.innerHTML = listItems;
-}
+};
 
-const array1 = ['hello', 'world', 'Kiev', 'Kharkiv', 'Odessa', 'Lviv'];
+const data = ['hello', 'world', 'Kiev', 23, ['never'], 'Kharkiv', 'Odessa', 'Lviv'];
+list(data);
 
+const deleteelements = document.createElement('div');
+deleteelements.textContent = 'Сторінка буде очищена через 10 секунд';
+document.body.appendChild(deleteelements);
 
-displayList(array1); // вызываем функцию для вывода первого массива
-
-
-// Для продвинутого задания можно использовать setTimeout
-let countdown = 10;
+let secondsLeft = 10;
 const countdownInterval = setInterval(() => {
-    countdown--;
-    if (countdown === 0) {
+    secondsLeft--;
+    deleteelements.textContent = `Сторінка буде очищена через ${secondsLeft} секунд`;
+    if (secondsLeft === 0) {
         clearInterval(countdownInterval);
-        document.getElementById('listContainer').innerHTML = ''; // Очищаем список
-    } else {
-        document.getElementById('countdown').innerText = countdown;
+        document.body.innerHTML = '';
     }
 }, 1000);
